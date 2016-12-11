3e80e4157a8290ca6e1e32edfa9eb38ab98c320c
Nathan Osman <nathan@quickmediasolutions.com>
2016-12-10 22:26:39 -0800

    Add coordinator class.

---
+++ src/coordinator.cpp
@@ -0,0 +1,50 @@
+/*
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2016 Nathan Osman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#include "coordinator.h"
+
+Coordinator::~Coordinator()
+{
+    qDeleteAll(*this);
+}
+
+void Coordinator::addClient(Client *client, int roomId)
+{
+    Room *room = value(roomId);
+
+    // If the room does not exist, create it
+    if (!room) {
+        room = new Room(roomId);
+        insert(roomId, room);
+    }
+
+    // Add the client to the room
+    room->addClient(client);
+}
+
+void Coordinator::onRoomEmptied()
+{
+    // Delete the room from the map
+    delete take(qobject_cast<Room*>(sender())->roomId());
+}
